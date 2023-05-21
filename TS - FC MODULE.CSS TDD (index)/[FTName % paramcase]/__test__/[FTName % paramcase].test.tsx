bac8c84eb1a4ca7a830e66e9bdab74a5a93e2524
Harconnen <harconnen.public@gmail.com>
2023-05-21 21:15:45 +0300

    fix

--- TS - FC MODULE.CSS TDD (index)/[FTName % paramcase]/__test__/[FTName % paramcase].test.tsx
+++ TS - FC MODULE.CSS TDD (index)/[FTName % paramcase]/__test__/[FTName % paramcase].test.tsx
@@ -6,7 +6,7 @@ describe('/PATH/<FTName % pascalcase>', () => {
   it('Название', () => {
     render(<<FTName % pascalcase>>test</<FTName % pascalcase>>);
     const element = screen.getByTestId(/<FTName % pascalcase>/i);
-    expect(baseElement).toBeTruthy();
+    expect(element).toBeTruthy();
     // expect(element).toBeInTheDocument();
     // expect(element).toMatchSnapshot();
   });
