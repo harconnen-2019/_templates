d9800301345ff0428c0b24287656bbc9f3bb8bc9
Harconnen <harconnen.public@gmail.com>
2023-09-23 11:41:20 +0300

    Добавление новых модулей

---
+++ TS - FC HOOK/hooks/use-[FTName % paramcase].tsx
@@ -0,0 +1,15 @@
+import { useEffect } from 'react';
+
+type <FTName % pascalcase>Props = {
+  callback: React.Dispatch<number>
+};
+
+function use<FTName % pascalcase>( {callback} : <FTName % pascalcase>Props) {
+  
+  useEffect(() => {
+    
+  }, [callback]);
+
+}
+
+export { use<FTName % pascalcase> }
